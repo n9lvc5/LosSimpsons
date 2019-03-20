4b703928804d110cfe72af0033aace715904fe8c
n9lvc5 <48769137+n9lvc5@users.noreply.github.com>
2019-03-20 13:09:20 -0500

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -1,11 +1,11 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 
-...
+
 
 // Update style using javascript
 let playerElement = document.getElementsByTagName("cast-media-player")[0];
-playerElement.style.setProperty('--splash-image', 'url("http://some/other/image.png")');
+playerElement.style.setProperty('--splash-image', 'url("https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png")');
 
-...
+
 
 context.start();
